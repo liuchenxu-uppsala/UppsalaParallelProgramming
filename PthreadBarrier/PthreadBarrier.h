4a6a96c420add1aa705a24451c3b0d81f4581091
chenxu.liu <liucxu@gmail.com>
2025-09-30 15:54:07 +0200

    Clean

--- PthreadBarrier/PthreadBarrier.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Chenx on 2025/9/30.
-//
-
-#ifndef PTHREADBARRIER_PTHREADBARRIER_H
-#define PTHREADBARRIER_PTHREADBARRIER_H
-void initBarrier(int threadCount);
-void barrier(int threadId);
-#endif //PTHREADBARRIER_PTHREADBARRIER_H
